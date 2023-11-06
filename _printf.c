3638cccb368a59437a8c210089d2773658caa138
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-05 21:37:01 -0600

    _printf filled out, needs testing

--- _printf.c
+++ _printf.c
@@ -1,4 +1,24 @@
 int _printf(const char *format)
 {
-    
+    int trace =0;
+
+    va_list args;
+    va_start(args, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            trace += (get_func(*format)(args));
+            format++;
+        }
+        else
+        {
+            _putchar(*format);
+            trace += 1;
+            format++;
+        }
+    }
+    return (trace);
 }
