a04e4142b92184f63afbac8d802859d54cbe1bf8
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2023-11-06 23:14:18 -0600

    made changes

--- print_int.c
+++ print_int.c
@@ -14,41 +14,33 @@ int print_int(va_list args)
 	int abcalc, abvault, track_dig;
 	int value = va_arg(args, int);
 
-	if (value == INT_MIN)
+	abcalc = value;
+	if (valve < 0)
 	{
 		putchar ('-');
 		i++;
-		abcalc = INT_MAX;
-	}
-	else if (value < 0)
-	{
-		putchar ('-');
-		i++;
-		abcalc = value * -1;
-	}
-	else
-	{
-		abcalc = value;
+		if (value == INT_MIN)
+			abcalc = INT_MAX;
+		else
+			abcalc = value * -1;
 	}
 	abvault = abcalc;
 	track_dig = 1;
-
 	while (abvault > 9)
 	{
 		abvault = abvault / 10;
-		track_dig = track_dig  * 10;
+		track_dig = track_dig * 10;
 	}
 	while (track_dig >= 1)
 	{
-		if (value == INT_MIN && track_dig == 1)
+		i++;
+		if (value == INT_MIN && tracK_dig == 1)
 		{
 			putchar('8');
-			i++;
 			track_dig = track_dig / 10;
 		}
 		else
 		{
-			i++;
 			putchar(((abcalc / track_dig) % 10) + '0');
 			track_dig = track_dig / 10;
 		}
