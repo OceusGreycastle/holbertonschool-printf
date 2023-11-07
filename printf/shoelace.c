9c4c8e79f1e3043dd5cb72bba8aacc39825dfaae
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2023-11-06 20:24:18 -0600

    added file "shoelace"

---
+++ printf/shoelace.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * shoelace - prints a string
+ * @args: arguments that format the data type
+ * Return: characters printed
+*/
+int shoelace( va_list args)
+{
+	char *str = va_arg(args, char*);
+	
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	fputs(str, stdout);
+	return (strlen(str));
+}
